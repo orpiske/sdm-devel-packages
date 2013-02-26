ad164cbc38af46752b91347c8a0c33ad1e0d7572
Otavio R. Piske <angusyoung@gmail.com>
2013-02-26 00:30:40 -0300

    Implemented a configuration script for Apache Maven

--- repository/org.apache/apache-maven/3.0.5/pkg/apache-maven.groovy
+++ repository/org.apache/apache-maven/3.0.5/pkg/apache-maven.groovy
@@ -1,4 +1,14 @@
+
+import net.orpiske.sdm.common.InstallDirUtils;
+import net.orpiske.sdm.common.WorkdirUtils;
+import net.orpiske.sdm.lib.io.IOUtil;
 import net.orpiske.sdm.packages.BinaryPackage;
+
+import static net.orpiske.sdm.lib.net.Downloader.*;
+import static net.orpiske.sdm.lib.Unpack.*;
+import static net.orpiske.sdm.lib.OsUtils.*;
+import static net.orpiske.sdm.lib.io.IOUtil.*;
+import static net.orpiske.sdm.lib.Executable.*;
 
 class ApacheMaven extends BinaryPackage {
 	def version = "3.0.5"
@@ -6,21 +16,33 @@ class ApacheMaven extends BinaryPackage {
 	
 	def url = "http://apache.mirror.pop-sc.rnp.br/apache/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz"
 
-	@Override
 	void fetch(String url) {
 		super.fetch(url)
+	
 		
-		println "Fetching ${url}"	
+		println "Fetching ${url}"
+		download(url)		
 	}
 	
-	@Override
-	void verify(String artifactName) {
-		println "Verifying ${artifactName}"
+	
+	void extract(String artifactName) {
+		unpack(artifactName)
 	}
 	
 	
 	void install(String artifactName) {
-		println "Installing ${artifactName}"
+		String installdir = InstallDirUtils.getInstallDir()
+		String workdir = WorkdirUtils.getWorkDir()
+		
+		shield("${installdir}/${name}-${version}/conf/settings.xml")
+		IOUtil.copy("${workdir}/${name}-${version}", "${installdir}/${name}-${version}");
+		
+		println "Installing ${workdir}/${name}-${version} to ${installdir}"
+		
+		if (isNix()) {
+			println "Creating symlinks"
+			exec("/bin/ln", "-sf ${installdir}/${name}-${version} ${installdir}/${name}")
+		}
 	}	
 
 	
