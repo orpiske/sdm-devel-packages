f136ad28520efe0a911742c7a516ae6a518052b7
Otavio R. Piske <angusyoung@gmail.com>
2013-03-07 23:09:07 -0300

    Code cleanup: removed unused imports

--- packages/org.apache/apache-maven/3.0.5/pkg/apache-maven.groovy
+++ packages/org.apache/apache-maven/3.0.5/pkg/apache-maven.groovy
@@ -1,9 +1,5 @@
 
-import net.orpiske.ssps.common.repository.utils.InstallDirUtils;
-import net.orpiske.sdm.common.WorkdirUtils;
-import net.orpiske.sdm.lib.io.IOUtil;
 import net.orpiske.sdm.packages.BinaryPackage;
-
 
 import static net.orpiske.sdm.lib.OsUtils.*;
 import static net.orpiske.sdm.lib.io.IOUtil.*;
@@ -14,18 +10,18 @@ class ApacheMaven extends BinaryPackage {
 	def version = "3.0.5"
 	def name = "apache-maven"
 	def url = "http://apache.mirror.pop-sc.rnp.br/apache/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz"
-	
+
 	void install() {
-		shield("${installDir}/${name}-${version}/conf/settings.xml")		
-		
+		shield("${installDir}/${name}-${version}/conf/settings.xml")
+
 		performInstall("${name}", "${version}")
-		
+
 		if (isNix()) {
 			println "Creating symlinks"
 			exec("/bin/ln", "-sf ${installDir}/${name}-${version} ${installDir}/${name}")
 		}
 	}
-	
+
 	void uninstall() {
 		if (isNix()) {
 			println "Removing symlinks"
