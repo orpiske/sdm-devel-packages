1d42a3f8af3da829943ad1e86c6ca730a40916b5
Otavio R. Piske <angusyoung@gmail.com>
2013-03-08 19:01:51 -0300

    Added a package for Maven 3.0.4 (in order to test upgrade functions)

---
+++ packages/org.apache/apache-maven/3.0.4/pkg/apache-maven.groovy
@@ -0,0 +1,31 @@
+
+import net.orpiske.sdm.packages.BinaryPackage;
+
+import static net.orpiske.sdm.lib.OsUtils.*;
+import static net.orpiske.sdm.lib.io.IOUtil.*;
+import static net.orpiske.sdm.lib.Core.*;
+import static net.orpiske.sdm.lib.Executable.*;
+
+class ApacheMaven extends BinaryPackage {
+	def version = "3.0.4"
+	def name = "apache-maven"
+	def url = "http://apache.mirror.pop-sc.rnp.br/apache/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz"
+
+	void install() {
+		shield("${installDir}/${name}-${version}/conf/settings.xml")
+
+		performInstall("${name}", "${version}")
+
+		if (isNix()) {
+			println "Creating symlinks"
+			exec("/bin/ln", "-sf ${installDir}/${name}-${version} ${installDir}/${name}")
+		}
+	}
+
+	void uninstall() {
+		if (isNix()) {
+			println "Removing symlinks"
+			exec("/bin/unlink", "${installDir}/${name}")
+		}
+	}
+}
