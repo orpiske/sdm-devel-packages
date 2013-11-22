564ebb124659247800459320318bd586058b6721
Otavio R. Piske <angusyoung@gmail.com>
2013-11-21 22:25:18 -0200

    Added a sample package to show how to compile package from source

--- packages/net.orpiske/sdm-common/0.3.0-SNAPSHOT/pkg/sdm-common.groovy
+++ packages/net.orpiske/sdm-common/0.3.0-SNAPSHOT/pkg/sdm-common.groovy
@@ -6,16 +6,15 @@ import org.apache.commons.io.FileUtils;
 
 import static net.orpiske.sdm.lib.OsUtils.*
 import static net.orpiske.sdm.lib.Core.*;
-import static net.orpiske.sdm.lib.Executable.*
-import static net.orpiske.sdm.lib.Unpack.unpack;
+import static net.orpiske.sdm.lib.Executable.*;
 
-class sdm extends SourcePackage {
+class SDMCommon extends SourcePackage {
     def version = "0.3.0-SNAPSHOT"
-    def name = "sdm"
-    def url = "https://github.com/orpiske/sdm.git"
+    def name = "sdm-common"
+    def url = "https://github.com/orpiske/sdm-common.git"
 
-	def dependencies = [ "net.orpiske/ssps-common":"(0.3.0-SNAPSHOT, 0.3.99)",
-						"net.orpiske/sdm-common":"(0.3.0-SNAPSHOT, 0.3.99)"]
+	def dependencies = [ "net.orpiske/ssps-common":"(0.3.0-SNAPSHOT, 0.3.99)"]
+
     def File buildDir = new File("${workDir}/${name}-${version}-build")
 
     void fetch(final String url) {
@@ -30,30 +29,26 @@ class sdm extends SourcePackage {
         repository.checkout(url, buildDir.getPath())
     }
 
+
     @Override
     void build() {
         ProjectBuilder builder = new MavenBuilder(buildDir)
 
-        builder.setProfile("Delivery")
-        if (builder.createPackage() == 0) {
+		println "Setting local Maven repository to " + workDir + File.separator + "m2repo"
+		builder.setDefine("maven.repo.local", workDir + File.separator + "m2repo")
+
+        if (builder.install() == 0) {
             println "Project ${name} compiled successfully"
         }
+
+
     }
 
     void install() {
-		unpack(buildDir.getPath() + File.separator + "target" + File.separator +
-				"${name}-${version}-bin.tar.gz", installDir)
-
-        if (isNix()) {
-            println "Creating symlinks"
-            exec("/bin/ln", "-sf ${installDir}/${name}-${version} ${installDir}/${name}")
-        }
+      // no-op
     }
 
     void uninstall() {
-        if (isNix()) {
-            println "Removing symlinks"
-            exec("/bin/unlink", "${installDir}/${name}")
-        }
+		// no-op
     }
 }
